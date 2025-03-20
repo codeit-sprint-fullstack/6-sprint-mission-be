21d00611ebaf6d44ae6bf50e87a1dfc065f53594
rakaso598 <nexxus86@outlook.com>
2025-03-20 13:52:39 +0900

    feat: 게시물 개별조회 & 게시물 목록조회

--- src/modules/articles.module.js
+++ src/modules/articles.module.js
@@ -19,4 +19,44 @@ articlesRouter.post("/", async (req, res, next) => {
   }
 });
 
+/**
+ * 게시물 조회
+ */
+articlesRouter.get("/:articleId", async (req, res, next) => {
+  try {
+    const articleId = Number(req.params.articleId);
+    if (articleId === NaN) throw new Error("articleId must be number");
+
+    const article = await prisma.article.findUnique({
+      where: { id: articleId },
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        createdAt: true,
+      },
+    });
+    if (!article) throw new Error("No article found");
+
+    res.status(200).json(article);
+  } catch (e) {
+    next(e);
+  }
+});
+
+/**
+ * 게시물 목록 조회
+ */
+articlesRouter.get("/", async (req, res, next) => {
+  try {
+    const articles = await prisma.article.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    res.json(articles);
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = articlesRouter;
