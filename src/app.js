eab6f3f3a8aaeee9f6d336fa4888d5b3bc221e06
fiivxyxxng <scx200800@gmail.com>
2025-04-16 11:14:14 +0900

    cors 설정

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = require("./modules/index.module");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5050;
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(router);
