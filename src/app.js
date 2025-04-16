5a9cba21f419abc09d1c216b17fa1d0ca523d7b5
jyeon03 <bjlyeon2003@naver.com>
2025-04-16 13:38:22 +0900

    cors 추가

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const router = require("./modules/index.module");
 
 const app = express();
 const PORT = 5050;
+const cors = require("cors");
+
+app.use(cors()); // CORS 허용 (프론트와 포트 다를 경우 필수)
 
 app.use(express.json()); // json 파싱을 해줘야함.
 app.use(router);
