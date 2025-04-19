82bd24eb4739bdbad1f7792a76bd720b7811e046
fiivxyxxng <scx200800@gmail.com>
2025-04-20 07:29:46 +0900

    cors 전체 허용

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = require("./modules/index.module");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
-const corsOptions = {
-  origin: ["http://localhost:3000"],
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
