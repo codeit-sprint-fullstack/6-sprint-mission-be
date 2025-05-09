115c297d4694f47d80ceb8a0ae4a831569507add
Decal <vumublne@naver.com>
2025-05-09 16:11:29 +0900

    feat: 상품 댓글 수정, 삭제 라우트 추가

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -148,4 +148,56 @@ commentRouter.post(`${PRODUCT_COMMENT}`, async (req, res, next) => {
   }
 });
 
+// 상품 댓글 수정
+commentRouter.patch(`${PRODUCT_COMMENT}/:commentId`, async (req, res, next) => {
+  try {
+    const productId = Number(req.params.productId);
+    const commentId = Number(req.params.commentId);
+    const { content } = req.body;
+
+    await prisma.$transaction(async (tx) => {
+      const productComment = await tx.productComment.findUnique({
+        where: { productId, id: commentId },
+      });
+      if (!productComment) throw new Error("존재하지 않는 댓글입니다.");
+
+      const updateProductComment = await tx.productComment.update({
+        where: { productId, id: commentId },
+        data: { content },
+      });
+
+      res.status(200).json(updateProductComment);
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+// 상품 댓글 삭제
+commentRouter.delete(
+  `${PRODUCT_COMMENT}/:commentId`,
+  async (req, res, next) => {
+    const productId = Number(req.params.productId);
+    const commentId = Number(req.params.commentId);
+    try {
+      await prisma.$transaction(async (tx) => {
+        const productComment = await tx.productComment.findUnique({
+          where: { productId, id: commentId },
+        });
+        if (!productComment) {
+          throw new Error("이미 삭제된 댓글입니다.");
+        }
+
+        await tx.productComment.delete({
+          where: { productId, id: commentId },
+        });
+
+        res.sendStatus(204);
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
 export default commentRouter;
