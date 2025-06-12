bb7c512efad5951210efd405970dd25a702f74f0
Decal <vumublne@naver.com>
2025-06-12 22:44:14 +0900

    feat: prisma 디렉토리 src로 이동 및 ts 마이그레이션

--- src/prisma/mock/productMock.ts
+++ src/prisma/mock/productMock.ts
@@ -1,70 +1,101 @@
-const PRODUCT_MOCK = [
+type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  authorId: string;
+}[];
+
+type TProductComment = {
+  content: string;
+  productId: number;
+  authorId: string;
+}[];
+
+type TProductLike = {
+  userId: string;
+  productId: number;
+}[];
+
+export const PRODUCT_MOCK: TProduct = [
   {
     name: "상품 시드 데이터 1",
     description: "상품 설명 시드 데이터 1",
     price: 10000,
+    authorId: "임시",
   },
   {
     name: "상품 시드 데이터 2",
     description: "상품 설명 시드 데이터 2",
     price: 20000,
+    authorId: "임시",
   },
   {
     name: "상품 시드 데이터 3",
     description: "상품 설명 시드 데이터 3",
     price: 30000,
+    authorId: "임시",
   },
   {
     name: "상품 시드 데이터 4",
     description: "상품 설명 시드 데이터 4",
     price: 40000,
+    authorId: "임시",
   },
   {
     name: "상품 시드 데이터 5",
     description: "상품 설명 시드 데이터 5",
     price: 50000,
+    authorId: "임시",
   },
 ];
 
-const PRODUCT_COMMENT_MOCK = [
+export const PRODUCT_COMMENT_MOCK: TProductComment = [
   {
     content: "상품 댓글 시드 데이터 1",
     productId: 1,
+    authorId: "임시",
   },
   {
     content: "상품 댓글 시드 데이터 2",
     productId: 2,
+    authorId: "임시",
   },
   {
     content: "상품 댓글 시드 데이터 3",
     productId: 3,
+    authorId: "임시",
   },
   {
     content: "상품 댓글 시드 데이터 4",
     productId: 4,
+    authorId: "임시",
   },
   {
     content: "상품 댓글 시드 데이터 5",
     productId: 5,
+    authorId: "임시",
   },
 ];
 
-const PRODUCT_LIKE_MOCK = [
+export const PRODUCT_LIKE_MOCK: TProductLike = [
   {
+    userId: "1",
     productId: 1,
   },
   {
+    userId: "2",
     productId: 2,
   },
   {
+    userId: "3",
     productId: 3,
   },
   {
+    userId: "4",
     productId: 4,
   },
   {
+    userId: "5",
     productId: 5,
   },
 ];
-
-export { PRODUCT_MOCK, PRODUCT_COMMENT_MOCK, PRODUCT_LIKE_MOCK };
---
+++ src/prisma/mock/userMock.ts
@@ -0,0 +1,33 @@
+type TUser = {
+  email: string;
+  password: string;
+  nickname: string;
+}[];
+
+export const USER_MOCK: TUser = [
+  {
+    email: "test1@test.com",
+    password: "11111111",
+    nickname: "테스터 1",
+  },
+  {
+    email: "test2@test.com",
+    password: "22222222",
+    nickname: "테스터 2",
+  },
+  {
+    email: "test3@test.com",
+    password: "33333333",
+    nickname: "테스터 3",
+  },
+  {
+    email: "test4@test.com",
+    password: "44444444",
+    nickname: "테스터 4",
+  },
+  {
+    email: "test5@test.com",
+    password: "55555555",
+    nickname: "테스터 5",
+  },
+];
