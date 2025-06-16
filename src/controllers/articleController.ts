32b724507d686018b15219e89114839888c77bdb
gksktl111 <gksktl111@naver.com>
2025-06-16 11:05:14 +0900

    refactor : 게시글 반환값 수정

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -54,7 +54,7 @@ const getArticleById = async (
     const userId = req.auth?.userId;
     const article = await articleService.getArticleById(articleId, userId);
 
-    res.status(200).json({ article });
+    res.status(200).json({ data: article });
   } catch (error) {
     next(error);
   }
@@ -76,7 +76,7 @@ const createArticle: RequestHandler = async (req, res, next) => {
     });
     res
       .status(201)
-      .json({ message: "게시글이 성공적으로 등록되었습니다.", article });
+      .json({ message: "게시글이 성공적으로 등록되었습니다.", data: article });
   } catch (error) {
     next(error);
   }
@@ -119,7 +119,7 @@ const updateArticle: RequestHandler = async (req, res, next) => {
 
     res.status(200).json({
       message: "게시글이 성공적으로 수정되었습니다.",
-      article,
+      data: article,
     });
   } catch (error) {
     next(error);
