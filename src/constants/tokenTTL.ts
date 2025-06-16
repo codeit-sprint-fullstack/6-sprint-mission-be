b183b8fbe06c4f327b13511b4ea44f6c9997cec0
gksktl111 <gksktl111@naver.com>
2025-06-16 11:03:26 +0900

    Revert "refactor : 댓글 반환값 구조 변경"
    This reverts commit 95374ec3849df1b64f6a754882c102c855572640.

--- src/constants/tokenTTL.ts
+++ src/constants/tokenTTL.ts
@@ -1,5 +1,5 @@
 // constants/token.js
 export const ACCESS_TOKEN_TTL = "15m";
-export const REFRESH_TOKEN_TTL = "15d";
-export const REFRESH_TOKEN_TTL_MS = 1000 * 60 * 60 * 24 * 15;
+export const REFRESH_TOKEN_TTL = "10d";
+export const REFRESH_TOKEN_TTL_MS = 1000 * 60 * 60 * 24 * 14;
 export const SLIDING_THRESHOLD_SEC = 60 * 60 * 24 * 4;
