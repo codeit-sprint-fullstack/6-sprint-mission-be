ec89a35a4de5657bb72b207a5f13d54e340df2c9
rakaso598 <nexonaddress98@gmail.com>
2025-06-26 14:37:14 +0900

    feat: 프리즈마 시딩기능 추가

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -1,4 +1,6 @@
 "use strict";
+// src/controllers/user.controller.ts (재확인)
+// 이 파일에 RequestWithUser 인터페이스와 req as RequestWithUser가 적용되어 있어야 합니다.
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,12 +9,14 @@ exports.getMyFavorites = exports.getMyProducts = exports.updatePassword = export
 const prismaClient_1 = __importDefault(require("../models/prisma/prismaClient"));
 const catchAsync_1 = __importDefault(require("../utils/catchAsync"));
 exports.getMe = (0, catchAsync_1.default)(async (req, res) => {
-    res.send(req.user);
+    const typedReq = req; // 이 부분이 있는지 확인
+    res.send(typedReq.user);
 });
 exports.updateMe = (0, catchAsync_1.default)(async (req, res) => {
+    const typedReq = req; // 이 부분이 있는지 확인
     const updatedUser = await prismaClient_1.default.user.update({
-        where: { id: req.user.id },
-        data: req.body,
+        where: { id: typedReq.user.id },
+        data: typedReq.body,
     });
     res.send(updatedUser);
 });
@@ -20,8 +24,9 @@ exports.updatePassword = (0, catchAsync_1.default)(async (req, res) => {
     res.send({ message: 'Password updated' });
 });
 exports.getMyProducts = (0, catchAsync_1.default)(async (req, res) => {
+    const typedReq = req; // 이 부분이 있는지 확인
     const products = await prismaClient_1.default.product.findMany({
-        where: { userId: req.user.id },
+        where: { userId: typedReq.user.id },
     });
     res.send(products);
 });
