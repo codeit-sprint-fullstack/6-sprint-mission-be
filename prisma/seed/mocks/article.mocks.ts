c9647e2e306fad3ced2b13dddd5a602ef10f32b9
fiivxyxxng <scx200800@gmail.com>
2025-07-06 22:16:24 +0900

    fix: mock 데이터 image 수정

--- prisma/seed/mocks/article.mocks.ts
+++ prisma/seed/mocks/article.mocks.ts
@@ -2,7 +2,8 @@ export const articleMocks = [
   {
     title: "오늘 날씨 어때요?",
     content: "비가 오려나 싶었는데 해가 떴네요.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 10,
     writerId: "user1",
     createdAt: "2025-04-16T10:00:00Z",
@@ -11,7 +12,8 @@ export const articleMocks = [
   {
     title: "React에서 상태 관리 어떻게 하세요?",
     content: "Redux랑 Recoil 중에 뭐가 나을까요?",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 212,
     writerId: "user1",
     createdAt: "2025-04-15T10:00:00Z",
@@ -20,7 +22,8 @@ export const articleMocks = [
   {
     title: "스터디 같이 하실 분!",
     content: "매주 수요일 저녁에 온라인 스터디 진행해요.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 331,
     writerId: "user1",
     createdAt: "2025-04-14T10:00:00Z",
@@ -29,7 +32,8 @@ export const articleMocks = [
   {
     title: "개발 슬럼프 극복 방법",
     content: "요즘 너무 하기 싫은데 다들 어떻게 극복하세요?",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 14,
     writerId: "user1",
     createdAt: "2025-04-13T10:00:00Z",
@@ -38,7 +42,8 @@ export const articleMocks = [
   {
     title: "넷플릭스 추천 좀!",
     content: "재밌게 본 드라마나 영화 추천해주세요~",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 10,
     writerId: "user2",
     createdAt: "2025-04-12T10:00:00Z",
@@ -47,7 +52,8 @@ export const articleMocks = [
   {
     title: "포트폴리오 피드백 받을 수 있을까요?",
     content: "링크 올릴게요, 피드백 부탁드려요!",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 52,
     writerId: "user2",
     createdAt: "2025-04-11T10:00:00Z",
@@ -56,7 +62,8 @@ export const articleMocks = [
   {
     title: "카페에서 코딩하기 좋은 곳",
     content: "서울에 괜찮은 카페 아시면 추천 부탁드려요!",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 654,
     writerId: "user2",
     createdAt: "2025-04-10T10:00:00Z",
@@ -65,7 +72,8 @@ export const articleMocks = [
   {
     title: "Mac vs Windows 개발환경",
     content: "개발자분들은 어떤 OS 쓰시나요?",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 546,
     writerId: "user2",
     createdAt: "2025-04-09T10:00:00Z",
@@ -74,7 +82,8 @@ export const articleMocks = [
   {
     title: "JS에서 깊은 복사하는 법",
     content: "lodash 말고 직접 구현해보려는데 어려워요.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 43,
     writerId: "user3",
     createdAt: "2025-04-08T10:00:00Z",
@@ -83,7 +92,8 @@ export const articleMocks = [
   {
     title: "자바스크립트 재미있어요",
     content: "처음엔 어렵더니 이제 좀 이해가 되네요!",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 87,
     writerId: "user3",
     createdAt: "2025-04-07T10:00:00Z",
@@ -92,7 +102,8 @@ export const articleMocks = [
   {
     title: "알고리즘 스터디 인원 구해요",
     content: "백준 골드 이상 목표입니다.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 44,
     writerId: "user3",
     createdAt: "2025-04-06T10:00:00Z",
@@ -101,7 +112,8 @@ export const articleMocks = [
   {
     title: "취준 스트레스 어떻게 푸세요?",
     content: "면접 앞두고 너무 떨려요ㅠㅠ",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 124,
     writerId: "user3",
     createdAt: "2025-04-05T10:00:00Z",
@@ -110,7 +122,8 @@ export const articleMocks = [
   {
     title: "이력서 양식 추천",
     content: "깔끔한 이력서 템플릿 있을까요?",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 125,
     writerId: "user4",
     createdAt: "2025-04-04T10:00:00Z",
@@ -119,7 +132,8 @@ export const articleMocks = [
   {
     title: "컴퓨터 책상 추천좀요",
     content: "책상이 너무 작아서 바꾸고 싶은데 추천좀요",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 51,
     writerId: "user4",
     createdAt: "2025-04-03T10:00:00Z",
@@ -128,7 +142,8 @@ export const articleMocks = [
   {
     title: "토이프로젝트 아이디어",
     content: "간단하면서도 임팩트 있는 거 없을까요?",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 25,
     writerId: "user4",
     createdAt: "2025-04-02T10:00:00Z",
@@ -137,7 +152,8 @@ export const articleMocks = [
   {
     title: "다들 몇 시에 코딩하세요?",
     content: "저는 밤 10시 이후가 집중 잘 되네요.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 2,
     writerId: "user4",
     createdAt: "2025-04-01T10:00:00Z",
@@ -146,7 +162,8 @@ export const articleMocks = [
   {
     title: "TypeScript 처음 써봤어요",
     content: "처음엔 헷갈리는데 확실히 안정성이 있네요.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 35,
     writerId: "user5",
     createdAt: "2025-03-31T10:00:00Z",
@@ -155,7 +172,8 @@ export const articleMocks = [
   {
     title: "다크모드 vs 라이트모드",
     content: "개발할 땐 다크모드, 눈은 아픈데 멋있어요.",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 15,
     writerId: "user5",
     createdAt: "2025-03-30T10:00:00Z",
@@ -164,7 +182,8 @@ export const articleMocks = [
   {
     title: "스터디 장소 추천",
     content: "오프라인으로 모임 가질만한 카페 있을까요?",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 11,
     writerId: "user5",
     createdAt: "2025-03-29T10:00:00Z",
@@ -173,7 +192,8 @@ export const articleMocks = [
   {
     title: "프론트엔드 로드맵 공유",
     content: "제가 정리한 로드맵 공유해요. 피드백 환영!",
-    image: "http://localhost:8080/image/f6abec9f77ac79750e12d9d1071215d6",
+    image:
+      "https://panda-market-bucket.s3.ap-northeast-2.amazonaws.com/public/1751807087648_img_default.png",
     likeCount: 9,
     writerId: "user5",
     createdAt: "2025-03-28T10:00:00Z",
