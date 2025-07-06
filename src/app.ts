1f1afd9eb0fa62bb0d7ef851a472dcc77cc13ed6
fiivxyxxng <scx200800@gmail.com>
2025-07-06 20:40:15 +0900

    chore: swagger api 적용, gitignore 수정

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,13 @@ import productRouter from "./routers/product.router";
 import commentRouter from "./routers/comment.router";
 import imageRouter from "./routers/image.router";
 import errorHandler from "./middlewares/errorHandler";
-// import { swaggerUi, specs } from "./swagger";
+import { swaggerUi, specs } from "./swagger";
 import "dotenv/config";
 import passport from "passport";
 import "./configs/passport.config";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.set("trust proxy", 1);
 app.use(passport.initialize());
@@ -33,10 +33,12 @@ app.use("/articles", articleRouter);
 app.use("/products", productRouter);
 app.use("/comments", commentRouter);
 app.use("/images", imageRouter);
-// app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
+
+export default app;
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,41 +1,43 @@
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerJsdoc = require("swagger-jsdoc");
-// import { fileURLToPath } from "url";
-// import path from "path";
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Panda Market API",
+      version: "1.0.0",
+      description: "swagger docs for panda market project",
+    },
+    components: {
+      securitySchemes: {
+        jwt: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        jwt: [],
+      },
+    ],
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "로컬 개발 서버",
+      },
+      {
+        url: "https://api.panda-market.store",
+        description: "배포 서버",
+      },
+    ],
+  },
+  apis: [path.resolve(__dirname, "swagger/*.yaml")],
+};
 
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Panda Market API",
-//       version: "1.0.0",
-//       description: "swagger docs for sprint mission 10",
-//     },
-//     components: {
-//       securitySchemes: {
-//         jwt: {
-//           type: "http",
-//           scheme: "bearer",
-//           bearerFormat: "JWT",
-//         },
-//       },
-//     },
-//     security: [
-//       {
-//         jwt: [],
-//       },
-//     ],
-//     host: "localhost:5050",
-//     basePath: "/",
-//   },
-//   apis: [path.resolve(__dirname, "swagger/*.yaml")],
-// };
+const specs = swaggerJsdoc(options);
 
-// const specs = swaggerJsdoc(options);
-
-// export { swaggerUi, specs };
+export { swaggerUi, specs };
